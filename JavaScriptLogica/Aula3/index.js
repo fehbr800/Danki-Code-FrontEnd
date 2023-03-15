c54923e34e0bbccb44af61c68d2ce5895e91463a
Matheus Emanoel <matheusemanoel1407@gmail.com>
2023-03-15 08:09:25 -0400

    aula03

---
+++ JavaScriptLogica/Aula3/index.js
@@ -0,0 +1,20 @@
+//Array
+            var nome = [];
+            nome[0] = 'Matheus';
+            nome[100] = 'Manel';
+
+            console.log(nome[0])
+
+//Objeto
+
+var animal = {};
+
+animal.nome = [];
+animal.peso = '24kg';
+animal.idade = 23;
+animal['idade'] = 24;
+
+animal.nome[0] = 'Boby';
+animal.nome[1] = 'Totô';
+
+console.log(animal)
