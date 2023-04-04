da0fa6038a6078059e68189ac0de5db58e153708
Matheus Emanoel <matheusemanoel1407@gmail.com>
2023-04-04 18:09:40 -0400

    projeto8

--- Projeto08/js/script.js
+++ Projeto08/js/script.js
@@ -1,4 +1,8 @@
-$('nav a').hover(function(){
+$('nav.desktop a').hover(function(){
     $('nav li').removeClass('selected');
     $(this).parent().addClass('selected');
+})
+
+$('nav.mobile h3').click(function(){
+    $('ul.nav-desktop').slideToggle();
 })
